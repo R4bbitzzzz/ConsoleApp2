6031f0690c80a0d9ac1c636c046e29ceaefd714a
Bottyán Vak <ecdl01@vbjnet.hu>
2023-10-26 09:19:34 +0200

    k

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -3,3 +3,21 @@ Random rnd = new Random();
 for (int i = 0; i < ints.Length; i++)
 {
     ints[i] = rnd.Next(1,1000);
+    
+}
+Console.WriteLine("A tömb elemeinek a kétszeresei: ");
+//Foreach - Változója csak egy másolat a tömb aktuális eleméről.
+foreach (int i in ints) {
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.Write($"{i} ");
+    Console.ForegroundColor= ConsoleColor.White;
+} 
+Console.WriteLine();
+Console.WriteLine("A tömb elemeinek a kétszeresei: ");
+foreach (int i in ints)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.Write($"{i * 2} ");
+    Console.ForegroundColor = ConsoleColor.White;
+}
+Console.WriteLine();
